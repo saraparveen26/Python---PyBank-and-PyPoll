9b9fd74bcc8ffd44bdc5565e6db56758419a8177
saraparveen26 <sara.parveen26@gmail.com>
2023-01-14 14:13:26 -0500

    Updated input source

---
+++ PyBank/main.py
@@ -0,0 +1,30 @@
+# First we'll import the os module
+# This will allow us to create file paths across operating systems
+import os
+
+# Module for reading CSV files
+import csv
+
+#     # Read the header row first (skip this step if there is now header)
+#     csv_header = next(csvreader)
+#     print(f"CSV Header: {csv_header}")
+
+# Set path for file
+csvpath = os.path.join ('Resources','budget_data.csv')
+
+# CSV reader specifies delimiter and variable that holds contents
+# # Open the CSV
+with open(csvpath,encoding='utf') as csvfile:
+    csvreader = csv.reader(csvfile, delimiter=",")
+    print(csvreader)
+
+# Read the header row first (skip this step if there is now header)
+    csv_header = next(csvreader)
+    print(f"CSV Header: {csv_header}")
+
+# Read each row of data after the header
+    for row in csvreader:
+        print(row)
+
+
+   
